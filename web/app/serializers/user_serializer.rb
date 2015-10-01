0fdaff142c17338aa3f7257c5f29bb6ae6e8f999
Melanie Freeman <mdf@andrew.cmu.edu>
2015-10-01 17:08:37 -0400

    Fixed user serializer bug

--- web/app/serializers/user_serializer.rb
+++ web/app/serializers/user_serializer.rb
@@ -1,6 +1,10 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :user_email, :user_token, :last_sign_in_at, :created_at
   
+  def user_email
+    object.email
+  end
+  
   def user_token
     if scope and scope.id == object.id
       object.authentication_token
